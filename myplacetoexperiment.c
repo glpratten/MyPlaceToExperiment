4c528993661f5f6ec78570e90a843381bb17c14c
Gianna Pratten <gianna.pratten@gmail.com>
2023-12-08 10:06:25 -0500

    learning to open file to read a value, if file exists

--- myplacetoexperiment.c
+++ myplacetoexperiment.c
@@ -5,6 +5,7 @@
 #define COUPONCOUNTERFILENAME "test.txt"
 
 int i;
+int filecounter = 0;
 FILE *experiment;   // create a FILE pointer variable
 char filename[255]; // create variable to store desired path/filename
 char testmoderesponse;
@@ -17,27 +18,50 @@ int main()
 
   if (testmoderesponse == TESTMODEON || testmoderesponse == TESTMODEON - 'a' + 'A') // recognize both upper and lower case 'y'
   {
-    printf("You chose to run the program. The file name is %s.\n", COUPONCOUNTERFILENAME);
+    printf("You chose to run the program in test mode. The file name is %s.\n", COUPONCOUNTERFILENAME);
     
-    experiment = fopen(COUPONCOUNTERFILENAME, "w"); // initialize file as write
-
-    if (experiment != NULL) 
-    {
-      printf("File created successfully!\n");
-      fclose(experiment);  // close the file
-    }
-    else 
-    {
-      printf("Failed to create the file.\n");
-    }
   }
   else if (testmoderesponse == TESTMODEOFF || testmoderesponse == TESTMODEOFF - 'a' + 'A')  // recognize both upper and lower case 'n'
   {
-    printf("You chose not to run the program.\n");
+    printf("You chose not to run the program in test mode.\n");
   }
   else
   {
     printf("Invalid response. Please enter 'y' or 'n'.\n");
+  }
+  experiment = fopen(COUPONCOUNTERFILENAME, "r+"); // initialize file as write
+  if (experiment == NULL)   // The file does not exist; create it and store the value 0
+  {
+    experiment = fopen(filename, "w");
+    if (experiemnt == NULL) 
+    {
+        printf("Error creating file");
+        return 1;
+    }
+    fprintf(experiment, "%d", 0);   // write the value 0 to the file
+    printf("File created, and value 0 is stored.\n");
+
+    // Close the file and reopen it in read mode
+    fclose(experiment);
+    experiment = fopen(filename, "r+");
+  }
+
+  if (experiment != NULL) 
+  {
+    fscanf(experiment, "%d", &filecounter);   // Read an integer from the file
+    printf("Read integer from the file: %d\n", filecounter);
+
+    filecounter++;   // Increment the integer by one
+
+    rewind(experiment);   // Move the file pointer back to the beginning of the file
+    fprintf(experiment, "%d", filecounter);    // Write the new value to the file
+    printf("Updated integer written to the file: %d\n", filecounter);
+    
+    fclose(experiment);  // close the file
+  }
+  else 
+  {
+    printf("Error opening the file.\n");
   }
   i++;
   
