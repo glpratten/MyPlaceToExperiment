faad91b8a53b13fda22c0141afa615c38aaaf070
Gianna Pratten <gianna.pratten@gmail.com>
2023-12-08 09:04:12 -0500

    added test mode

--- myplacetoexperiment.c
+++ myplacetoexperiment.c
@@ -1,27 +1,46 @@
 #include <stdio.h>
 
+#define TESTMODEOFF 0 // run program as normal
+#define TESTMODEON  1 // run program as a test
+
 int i;
 FILE *experiment;   // create a FILE pointer variable
 char filename[255]; // create variable to store desired path/filename
+char testmoderesponse;
 
 int main() 
 {
   
-  printf("Enter the path and filename: ");
-  scanf("%s", filename); // get file path & name from the user
-  printf(filename);
-  experiment = fopen(filename, "w"); // initialize file as read/write
+  printf("Do you wish to run this program in test mode? (y/n)\n");  // provide option to run in test mode. No coupon will be produced
+  scanf("%c", &testmoderesponse);
 
-  if (experiment != NULL) 
+  if (testmoderesponse == 'y' || testmoderesponse == 'Y')
   {
-    printf("File created successfully!\n");
-    fclose(experiment);  // close the file
+    printf("You chose to run the program.\n");
+    printf("Enter the path and filename: ");
+    scanf("%s", filename); // get file path & name from the user
+    printf(filename);
+    experiment = fopen(filename, "w"); // initialize file as read/write
+
+    if (experiment != NULL) 
+    {
+      printf("File created successfully!\n");
+      fclose(experiment);  // close the file
+    }
+    else 
+    {
+      printf("Failed to create the file.\n");
+    }
   }
-  else 
+  else if (testmoderesponse == 'n' || testmoderesponse == 'N')
   {
-    printf("Failed to create the file.\n");
+    printf("You chose not to run the program.\n");
   }
-   i++;
-   
-   return 0;
+  else
+  {
+    printf("Invalid response. Please enter 'y' or 'n'.\n");
+  }
+  i++;
+  
+  return 0;
 }
