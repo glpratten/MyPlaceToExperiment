fcde1e2883db2d690bde34f5669f61425f27b6ab
Gianna Pratten <gianna.pratten@gmail.com>
2023-12-12 16:00:13 -0500

    fixed for loop

--- myplacetoexperiment.c
+++ myplacetoexperiment.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
 
 #define LIMIT 3         // program can only run 3 times
-#define TESTMODEOFF 'n' // run program as normal
-#define TESTMODEON  'y' // run program as a test
+#define TEST_MODE_OFF 'n' // run program as normal
+#define TEST_MODE_ON  'y' // run program as a test
 #define FILENAME "file.txt" // file to track number of times program has been run
 
-int i;
-int filecounter = 0;
+int file_counter = 0;
 FILE *experiment;   // create a FILE pointer variable
-char testmoderesponse;
+char test_mode_response;
 
 int main() 
 {
   printf("Do you wish to run this program in test mode? (y/n)\n");  // provide option to run in test mode. No coupon will be produced
-  scanf("%c", &testmoderesponse);
+  scanf("%c", &test_mode_response);
 
-  if (testmoderesponse == TESTMODEON || testmoderesponse == TESTMODEON - 'a' + 'A') // recognize both upper and lower case 'y'
+  if (test_mode_response == TEST_MODE_ON || test_mode_response == TEST_MODE_ON - 'a' + 'A') // recognize both upper and lower case 'y'
   {
     printf("You chose to run the program in test mode.\n");
     return 0;
   }
-  else if (testmoderesponse == TESTMODEOFF || testmoderesponse == TESTMODEOFF - 'a' + 'A')  // recognize both upper and lower case 'n'
+  else if (test_mode_response == TEST_MODE_OFF || test_mode_response == TEST_MODE_OFF - 'a' + 'A')  // recognize both upper and lower case 'n'
   {
     printf("You chose not to run the program in test mode.\n");
   }
   else
   {
     printf("Invalid response. Please enter 'y' or 'n'.\n");
+    return 1;
   }
   experiment = fopen(FILENAME, "r+"); // initialize file as write
   if (experiment == NULL)   // The file does not exist; create it and store the value 0
@@ -50,14 +50,14 @@ int main()
   {
     printf("The file exists. Opening file named %s.\n", FILENAME);
 
-    fscanf(experiment, "%d", &filecounter);   // Read an integer from the file
-    printf("Read integer from the file: %d\n", filecounter);
+    fscanf(experiment, "%d", &file_counter);   // Read an integer from the file
+    printf("Read integer from the file: %d\n", file_counter);
 
-    filecounter++;   // Increment the integer by one
+    file_counter++;   // Increment the integer by one
 
     rewind(experiment);   // Move the file pointer back to the beginning of the file
-    fprintf(experiment, "%d", filecounter);    // Write the new value to the file
-    printf("Updated integer written to the file: %d\n", filecounter);
+    fprintf(experiment, "%d", file_counter);    // Write the new value to the file
+    printf("Updated integer written to the file: %d\n", file_counter);
     
     fclose(experiment);  // close the file
   }
@@ -66,10 +66,9 @@ int main()
     printf("Something went wrong with the file named %s.\n", FILENAME);
     printf("Error opening the file.\n");
   }
-  while (i < LIMIT)
+  for (int i=0; i < LIMIT; i++)
   {
-    printf("limit %d and i %d", LIMIT, i+1);
-    i++;
+    printf("This program is running %d of %d times allowed.\n", i+1, LIMIT);
   }
   
   return 0;
